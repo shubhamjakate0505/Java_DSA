cf357a7d22c5e87b0dec662412f12fc182515d98
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-08-27 22:50:55 +0530

    27/08/2024

--- Day_10_Heap/Heap/ImplementH.java
+++ Day_10_Heap/Heap/ImplementH.java
@@ -14,6 +14,10 @@ public class ImplementH{
                 int temp=arr.get(x);
                 arr.set(x,arr.get(par));
                 arr.set(par,temp); 
+
+
+                x=par;
+                par=(x-1)/2;
             }
 
 
@@ -22,9 +26,62 @@ public class ImplementH{
             return arr.get(0);
             
         }
+        private void heapify(int i){
+        int left=2*i+1;
+        int right=2*i+2;
+        int minidx=i;
+
+        if(left<arr.size() && arr.get(minidx)>arr.get(left)){
+            minidx=left;
+        }
+        if(right<arr.size() && arr.get(minidx)>arr.get(right)){
+            minidx=right;
+        }
+        
+        if(minidx !=i){
+            int temp=arr.get(i);
+            arr.set(i,arr.get(minidx));
+            arr.set(minidx,temp);
+
+            heapify(minidx);
+        }
     }
+
+    public int remove(){
+        int data=arr.get(0);
+
+        //step1 swap Frist and last
+        int temp=arr.get(0);
+        arr.set(0,arr.get(arr.size()-1));
+        arr.set(arr.size()-1,temp);
+
+        //step remove last element
+        arr.remove(arr.size()-1);
+        
+        //fixheap
+        heapify(0);
+        return data;
+
+    }
+    public boolean isEmpty(){
+        return arr.size()==0;
+    }
+    }
+
+
 
     public static void main(String args[]){
+        Heap h=new Heap();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
 
+        while(!h.isEmpty()){
+            System.out.println(h.peek());
+            h.remove();
+        }
     }
+
+    
 }

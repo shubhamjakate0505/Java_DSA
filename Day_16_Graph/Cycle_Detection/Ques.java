e4772470a4f61170ac47f0749615ee2a4e4ea389
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-09-09 00:34:35 +0530

    09/09/24

--- Day_16_Graph/Cycle_Detection/Ques.java
+++ Day_16_Graph/Cycle_Detection/Ques.java
@@ -1,3 +1,5 @@
+
+// import java.util.ArrayList;
 import java.util.*;
 public class Ques{
     public static class Edge{
@@ -16,34 +18,22 @@ public class Ques{
             graph[i]=new ArrayList<>();
         }
          //0 -vertax
-        graph[0].add(new Edge(0,1,1));
         graph[0].add(new Edge(0,2,1));
+       
         
         //1 -vertax
         graph[1].add(new Edge(1,0,1));
-        graph[1].add(new Edge(1,3,1));
+        
         
         //2 -vertax
-        graph[2].add(new Edge(2,0,1));
-        graph[2].add(new Edge(2,4,1));
+        graph[2].add(new Edge(2,3,1));
+        
 
         //3 -vertax
-        graph[3].add(new Edge(3,1,1));
-        graph[3].add(new Edge(3,4,1));
-        graph[3].add(new Edge(3,5,1));
+        graph[3].add(new Edge(3,0,1));
+      
 
-        //4 -vertax
-        graph[4].add(new Edge(4,2,1));
-        graph[4].add(new Edge(4,5,1));
-
-        //5 -vertax
-        graph[5].add(new Edge(5,6,1));
-        graph[5].add(new Edge(5,4,1));
-        graph[5].add(new Edge(5,3,1));
-
-        //6 -vertax
-        graph[6].add(new Edge(6,5,1));
-
+        
                        /* 
         1 - - - 3 
       /         |   \ 
@@ -58,33 +48,20 @@ public class Ques{
         
     } 
 
-    public static void bfs(ArrayList<Edge>[] graph){
-        Queue<Integer> q=new LinkedList<>();
-        boolean vis[]=new boolean[graph.length];
-        q.add(0);//source
-
-        while(!q.isEmpty()){//jab tak Q empty nhi hoth tab tak remove karo
-            int curr=q.remove();
-
-            if(!vis[curr]){//if curr not visited than vist  
-                    System.out.print(curr+" ");
-                    vis[curr]=true;
-                    for(int i=0;i<graph[curr].size();i++){
-                        Edge e=graph[curr].get(i);
-                        q.add(e.dest);
-                    }
-            }
-        }
+   
 
 
+    public static boolean isCycle(ArrayList<Edge>[] graph){
+      boolean vis[]=new boolean[graph.length];
+      boolean
     }
 
     public static void main(String args[]){
-       int v=7;
+       int v=4;
        ArrayList<Edge>[] graph=new ArrayList[v];//empty arraylist
-        creategraph(graph);  
+      creategraph(graph);  
 
-        bfs(graph);
+        
       
     }
 }

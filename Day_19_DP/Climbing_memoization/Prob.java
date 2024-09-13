03e876a0e0a5b45b088508c52f10518dfe066f2d
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-09-13 22:57:37 +0530

    13/09/2024

--- Day_19_DP/Climbing_memoization/Prob.java
+++ Day_19_DP/Climbing_memoization/Prob.java
@@ -1,23 +1,23 @@
 import java.util.*;
-public class Prob{
-    public static int countways(int n,int ways){
-        if(n==0){
+public class Prob {
+    public static int countways(int n, int ways[]) {
+        if (n == 0) {
             return 1;
-
         }
-        if(n<0){
+        if (n < 0) {
             return 0;
         }
-        if(ways[n]!=-1){//already calculated
+        if (ways[n] != -1) { // already calculated
             return ways[n];
         }
-        ways[n]=countways(n-1,ways)+countways(n-2,ways);
+        ways[n] = countways(n - 1, ways) + countways(n - 2, ways);
         return ways[n];
     }
-    public static void main(String args[]){
-        int n=5;
-        int ways[]=new int[n+1];
-        Arrays.fill(ways,-1);
-        System.out.println(countways(n));
+
+    public static void main(String args[]) {
+        int n = 5;
+        int ways[] = new int[n + 1];
+        Arrays.fill(ways, -1);
+        System.out.println(countways(n, ways)); // Passing the `ways` array
     }
 }

92ef4b6125e2b6ca826f85d6f3572c5e5391e00e
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-10-14 20:19:13 +0530

    14/08/2023

--- Day_29_LinkedList/zigZag/Prob.java
+++ Day_29_LinkedList/zigZag/Prob.java
@@ -1,6 +1,17 @@
 import java.util.*;
 public class Prob{
-    
+    public class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+        
+        
+    }
+    public static Node head;
+    public static Node tail;
     public void zigzag(){
             Node slow=head;
             Node fast=head.next;
@@ -19,10 +30,61 @@ public class Prob{
             while(curr !=null){
                 next=curr.next;
                 curr.next=prev;
+                prev=curr;
                 curr=next;
             }
-    }    
-    public static void main(String args[]){
 
+            Node left=head;
+            Node right=prev;
+            Node nextL,nextR;
+
+            //alt merge -zig-zag merge
+
+            while(left!=null && right !=null){
+                nextL=left.next;
+                left.next=right;
+                nextR=right.next;
+                right.next=nextL;
+                
+                left=nextL;
+                right=nextR;
+            }
+
+    }    
+        public void Print(){
+        if(head==null){
+            System.out.println("LL is empty");
+            return;
+        }
+
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("Null");
+    }
+     public void addLast(int data){
+        Node newnode=new Node(data);
+       
+        if(head==null){
+            head=tail=newnode;
+            return;
+        }
+        tail.next=newnode;
+        tail=newnode;
+    }
+    public static void main(String args[]){
+        Prob ll=new Prob();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
+
+        ll.Print();
+        ll.zigzag();
+        ll.Print();
     }
 }

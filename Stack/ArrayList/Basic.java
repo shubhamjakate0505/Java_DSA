d15f7ee97137cf52d551cb2229b60ab8368e3751
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-10-16 23:10:30 +0530

    16/10/2024

---
+++ Stack/ArrayList/Basic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Basic{
+    static class Stack{
+       static ArrayList<Integer> list=new ArrayList<>();
+
+       public static boolean isEmpty(){
+        return list.size()==0;
+       }
+       //push
+       public static void Push(int data){
+            list.add(data);
+       }
+    }
+    public static void main(String args[]){
+
+    }
+}
